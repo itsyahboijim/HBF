cbff0ac3927d9b83d93721020df42d0bb8ebef7b
itsyahboijim <69668111+itsyahboijim@users.noreply.github.com>
2022-12-07 15:56:00 +0800

    Modify sample data

--- src/database/database.ts
+++ src/database/database.ts
@@ -23,42 +23,54 @@ export class Database{
                 password: "ndhpass",
                 name: "Naic Doctors Hospital",
                 location: "Naic",
-                bedCount: 5
+                contactNum: "***********",
+                maxBeds: 50,
+                availableBeds: 5
             },
             {
                 email: "slrh@gmail.com",
                 password: "slrhpass",
                 name: "San Lorenzo Ruiz Hospital",
                 location: "Naic",
-                bedCount: 10
+                contactNum: "***********",
+                maxBeds: 25,
+                availableBeds: 10
             },
             {
                 email: "kpfh@gmail.com",
                 password: "kpfhpass",
                 name: "Korean Philippines Friendship Hospital",
                 location: "Trece",
-                bedCount: 12
+                contactNum: "***********",
+                maxBeds: 35,
+                availableBeds: 12
             },
             {
                 email: "geamh@gmail.com",
                 password: "geamhpass",
                 name: "Gen. Emilio Aguinaldo Memorial Hospital",
                 location: "Trece",
-                bedCount: 17
+                contactNum: "***********",
+                maxBeds: 20,
+                availableBeds: 17
             },
             {
                 email: "tsmc@gmail.com",
                 password: "tsmcpass",
                 name: "Tanza Specialist Medical Center",
                 location: "Tanza",
-                bedCount: 22
+                contactNum: "***********",
+                maxBeds: 33,
+                availableBeds: 22
             },
             {
                 email: "sndtmh@gmail.com",
                 password: "sndtmhpass",
                 name: "Sto. Nino De Tanza Medical Hospital",
                 location: "Tanza",
-                bedCount: 25
+                contactNum: "***********",
+                maxBeds: 55,
+                availableBeds: 25
             }
         ];
 
