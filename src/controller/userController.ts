b0fcb1510b7bb356d4b833d2777079a3993441f6
itsyahboijim <69668111+itsyahboijim@users.noreply.github.com>
2022-12-08 11:34:19 +0800

    Fix incorrect field input validation for both sign up and login

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -5,8 +5,15 @@ const db = new Database("hospitals");
 
 export async function login(req: Request, res: Response){
     const { email, password } = req.body as Record<string, any>;
-    const profile = await db.collection.findOne({email: email});
+    
+    if (!email || !password){
+        res.status(400).send({
+            success: false,
+            error: "Please fill in all the fields.",
+        });
+    }
 
+    const profile = await db.collection.findOne({email: email});
     // Check if a user was found
     if (!profile){
         res.status(400).send({
@@ -37,7 +44,7 @@ export async function register(req: Request, res: Response){
     const { email, password, name, location, contactNum, maxBeds, availableBeds } = req.body as Record<string, any>;
 
     // Check all fields 
-    if (!email && !password && !name && !location && !contactNum && !maxBeds){
+    if (!email || !password || !name || !location || !contactNum || !maxBeds){
         res.status(400).send({
             success: false,
             error: "Please fill in all the required fields.",
