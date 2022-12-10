2bfe9a3fce2409d577190b13f54d7e125cc89da5
itsyahboijim <69668111+itsyahboijim@users.noreply.github.com>
2022-12-10 09:38:00 +0800

    Revise regex for special characters and add email field validation

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,7 @@
 $(function(){
     const baseUrl = window.location.origin;
+    document.getElementById("loginForm").reset();
+    document.getElementById("signUpForm").reset();
 
     // Event listener for sign in
     document.getElementById("loginForm").addEventListener("submit", async (e) => {
@@ -22,6 +24,17 @@ $(function(){
         document.getElementById("loginError").innerText = resBody.error;
     });
 
+    let signUpEmailError = document.getElementById("emailError");
+    function checkEmail(e){
+        if (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(e.target.value)){
+            signUpEmailError.innerText = "";
+        }
+        else {
+            signUpEmailError.innerText = "Please enter a valid email address.";
+        }
+    }
+    document.getElementById("signUpEmail").addEventListener("input", checkEmail);
+    
     let passwordErrors = [];
     function hasUppercase(str){
         return (/[A-Z]/.test(str));
@@ -30,8 +43,7 @@ $(function(){
         return (/[a-z]/.test(str));
     }
     function hasSpecialChar(str){
-        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-        return specialChars.test(str);
+        return (/[^a-zA-Z0-9]/.test(str));
     }
     function checkPassword(e){
         $("#passwordErrors").empty();
@@ -63,6 +75,7 @@ $(function(){
         e.preventDefault();
 
         if (passwordErrors.length > 0) return;
+        if (signUpEmailError.innerText != "") return;
         
         const formData = formArrayToObject($("#signUpForm").serializeArray());
         const res = await fetch(baseUrl + "/api/register", {
