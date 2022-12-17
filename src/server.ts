eba460087324836f9982064bcb053ad133e77637
itsyahboijim <69668111+itsyahboijim@users.noreply.github.com>
2022-12-17 10:17:53 +0800

    Add maintainer email to greenlock

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,6 @@ const app = require("./index");
 require("greenlock-express").init({
     packageRoot: __dirname,
     configDir: "../greenlock.d",
+    maintainerEmail: "gironjim@gmail.com",
     cluster: false,
 }).serve(app);
